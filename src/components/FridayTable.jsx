46a47e26c3f043ef37c9d048d6d61c3b0777a054
Abdullah Al Akash <aabdullahalakash@gmail.com>
2024-03-23 23:06:53 +0600

    Update Friday Routine Done

--- src/components/FridayTable.jsx
+++ src/components/FridayTable.jsx
@@ -8,12 +8,11 @@ const FridayTable = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(
-      "https://routine-management-system-backend.onrender.com/api/v1/routine?day=Saturday&shift=Regular"
-    )
+    fetch("./Friday-Evening.json")
       .then((res) => res.json())
       .then((data) => {
         setData(data);
+        console.log(data);
         setLoading(false);
       });
   }, []);
@@ -161,118 +160,113 @@ const FridayTable = () => {
           </thead>
 
           {/* table body start here  */}
-          <tbody>
-            <tr>
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-black p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                Batch
-              </td>
-              <td
-                className="text-[14px] border-[#000]  border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center"
-                rowSpan={2}
-                colSpan={3}
-              >
-                Year/ Sem
-              </td>
-              <td
-                className="text-[14px] border-[#000]  border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center"
-                rowSpan={2}
-                colSpan={2}
-              >
-                Sem No
-              </td>
-              <td
-                className="text-[14px]  border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center"
-                rowSpan={2}
-                colSpan={2}
-              >
-                Room Number
-              </td>
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                09:00-09:50
-              </td>
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                9:50 - 10:40
-              </td>
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                10:50 - 11:40
-              </td>
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                11:40 - 12:30
-              </td>
+          {!loading &&
+            data?.data?.map((item, index) => {
+              const { _id, batch, courses, room, sem, yearSem } = item;
 
-              <td className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"></td>
-              {/* After Break */}
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                2:10 - 3:00
-              </td>
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                3:00 - 3:50
-              </td>
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                3:50 -4:40
-              </td>
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                4:50 - 5:40
-              </td>
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                5:40 - 6:30
-              </td>
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                6:50 - 7:40
-              </td>
-              <td
-                className="text-[14px] border-[#000] border-2 border-b-[1px] border-r-[1px] text-[#000] p-[16px] text-center whitespace-nowrap"
-                rowSpan={2}
-                colSpan={3}
-              >
-                7:40 - 8:30
-              </td>
-            </tr>
-          </tbody>
+              return (
+                <tr key={_id}>
+                  <td
+                    className={`px-[16px] border-r-[1px] bg-white py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
+                    colSpan={3}
+                  >
+                    {batch}th
+                  </td>
+                  <td
+                    className={`px-[16px] border-r-[1px] bg-white py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
+                    colSpan={3}
+                  >
+                    {yearSem}
+                  </td>
+                  <td
+                    className="px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]"
+                    colSpan={2}
+                  >
+                    {sem}
+                  </td>
+                  <td
+                    className="px-[
+                          16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]"
+                    colSpan={2}
+                  >
+                    {room}
+                  </td>
+                  <td
+                    colSpan={6}
+                    className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
+                  >
+                    {courses?.["1"]?.courseCode && (
+                      <>
+                        {courses?.["1"]?.courseCode} (
+                        {courses?.["1"]?.courseTitle}) {courses?.["1"]?.teacher}
+                      </>
+                    )}
+                  </td>
+                  <td
+                    colSpan={6}
+                    className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
+                  >
+                    {courses?.["3"]?.courseCode && (
+                      <>
+                        {courses?.["3"]?.courseCode} ({" "}
+                        {courses?.["3"]?.courseTitle}) {courses?.["3"]?.teacher}
+                      </>
+                    )}
+                  </td>
+                  <td className="px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 border-t-0 border-b-0 text-[20px]">
+                    {" "}
+                    {index === 4 ? "B" : ""} {index === 5 ? "R" : ""}{" "}
+                    {index === 6 ? "E" : ""} {index === 7 ? "A" : ""}{" "}
+                    {index === 8 ? "K" : ""}
+                  </td>
+                  <td
+                    colSpan={3}
+                    className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
+                  >
+                    {courses?.["5"]?.courseCode && (
+                      <>
+                        {courses?.["5"]?.courseCode} (
+                        {courses?.["5"]?.courseTitle}) {courses?.["5"]?.teacher}
+                      </>
+                    )}
+                  </td>
+                  <td
+                    colSpan={6}
+                    className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
+                  >
+                    {courses?.["6"]?.courseCode && (
+                      <>
+                        {courses?.["6"]?.courseCode} (
+                        {courses?.["6"]?.courseTitle}) {courses?.["6"]?.teacher}
+                      </>
+                    )}
+                  </td>
+                  <td
+                    colSpan={6}
+                    className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
+                  >
+                    {courses?.["8"]?.courseCode && (
+                      <>
+                        {courses?.["8"]?.courseCode} (
+                        {courses?.["8"]?.courseTitle}){courses?.["8"]?.teacher}
+                      </>
+                    )}
+                  </td>
+                  <td
+                    colSpan={6}
+                    className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
+                  >
+                    {courses?.["10"]?.courseCode && (
+                      <>
+                        {courses?.["10"]?.courseCode} (
+                        {courses?.["10"]?.courseTitle}){" "}
+                        {courses?.["10"]?.teacher}
+                      </>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
         </table>
       </div>
     </div>
